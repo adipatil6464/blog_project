f8ba556e2a69c93a8a711fbcc2833626443d34c8
adipatil6464 <91333594+adipatil6464@users.noreply.github.com>
2023-11-18 13:45:25 +0530

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'practice',
         'USER':'root',
         'PASSWORD':'adipatil@6464',
-        'HOST':'127.0.0.1',
+        'HOST':'0.0.0.0',
         'PORT':'3306',
     }
 }
